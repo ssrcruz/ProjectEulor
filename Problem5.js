0a46d0a6983dbbc89dd7bec3472e2b23a4b18aaf
ssrcruz <ssrcruz@gmail.com>
2018-01-08 06:03:22 +0000

    Add/Remove comments

--- Problem5.js
+++ Problem5.js
@@ -1,37 +1,29 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-const v8 = require('v8');
-const statistics = v8.getHeapStatistics();
-//console.log(statistics);
-
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-let smallestMultiple = () => {
-    let array = [];
+let min = 1;
+let max = 20;
+
+let smallestMultiple = (min, max) => {
     for (let i = 1;; i++) {
-        let someArray = [];
-        for (let k = 1; k <= 20; k++) {
-//            console.log(k);
+        let multiplesArray = [];
+        // If number is a multiple add number to multiplesArray
+        for (let k = min; k <= max; k++) {
             let num = i % k;
             if (num === 0) {
                 let data = {};
                 data[i] = data[i] || k;
                 data[i][k];
-                someArray.push(data);
-//                array.push(data);
+                multiplesArray.push(data);
             }
         }
-//        console.log(someArray.length, 20);
-        if (someArray.length === 20) {
-            console.log(i, someArray);
+        // If array length is equal to the max, 
+        // then you've found the smallest number that evenly divides between all numbers min and max!
+        if (multiplesArray.length === max) {
+            console.log(i, multiplesArray);
             break;
         }
         
     }
-//    console.log(array);
 };
 
-smallestMultiple();
+smallestMultiple(min, max);
